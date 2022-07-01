7654ba497c3d78b0552791bf239c9bd08f389ace
Vasiliy <45902581+call-me-Neuro@users.noreply.github.com>
2022-07-01 11:33:02 +0300

    Update speed_test.py

--- speed_test.py
+++ speed_test.py
@@ -9,7 +9,7 @@ class Test():
         self.resultsTwo = []
         
     def test(self):
-        t11 = time.time()
+        t3 = time.time()
         for i in range(self.times):
             t1 = time.time()
             self.mainOne()
@@ -22,21 +22,21 @@ class Test():
             timer2 = time.time() - t2
             self.resultsTwo.append(timer2)
 
-        timer3 = time.time() - t11
+        timer3 = time.time() - t3
         maks = max(self.resultsTwo)-max(self.resultsOne)
         minn = min(self.resultsTwo)-min(self.resultsOne)
         meann = mean(self.resultsTwo)-mean(self.resultsOne)
         
         result = f"""{self.times} times
 ITERATION WITHOUT INDICES
-max: {max(self.resultsOne)} #{'+'+str(maks*-1) if maks*-1 > 0 else maks*-1} {str(max(self.resultsTwo) / max(self.resultsOne) * 100 - 100)[:4]+'%' if max(self.resultsTwo) > max(self.resultsOne) else ''}
-min: {min(self.resultsOne)} #{'+'+str(minn*-1) if minn*-1 > 0 else minn*-1} {str(min(self.resultsTwo) / min(self.resultsOne) * 100 - 100)[:4]+'%' if min(self.resultsTwo) > min(self.resultsOne) else ''}
-mean: {mean(self.resultsOne)} #{'+'+str(meann*-1) if meann*-1 > 0 else meann*-1} {str(mean(self.resultsTwo) / mean(self.resultsOne) * 100 - 100)[:4]+'%' if mean(self.resultsTwo) > mean(self.resultsOne) else ''}
+max: {max(self.resultsOne)} #{'+'+str(maks*-1) if maks*-1 > 0 else maks*-1} {'+'+str(max(self.resultsOne) / max(self.resultsTwo) * 100 - 100)[:4] if max(self.resultsOne) > max(self.resultsTwo) else ''}
+min: {min(self.resultsOne)} #{'+'+str(minn*-1) if minn*-1 > 0 else minn*-1} {'+'+str(min(self.resultsOne) / min(self.resultsTwo) * 100 - 100)[:4] if min(self.resultsOne) > min(self.resultsTwo) else ''}
+mean: {mean(self.resultsOne)} #{'+'+str(meann*-1) if meann*-1 > 0 else meann*-1} {'+'+str(mean(self.resultsOne) / mean(self.resultsTwo) * 100 - 100)[:4]+'%' if mean(self.resultsOne) > mean(self.resultsTwo) else ''}
 
 ITERATION WITH INDICEC
-max: {max(self.resultsTwo)} #{'+'+str(maks) if maks > 0 else maks} {str(max(self.resultsOne) / max(self.resultsTwo) * 100 - 100)[:4]+'%' if max(self.resultsOne) > max(self.resultsTwo) else ''}
-min: {min(self.resultsTwo)} #{'+'+str(minn) if minn > 0 else minn} {str(min(self.resultsOne) / min(self.resultsTwo) * 100 - 100)[:4]+'%' if min(self.resultsOne) > min(self.resultsTwo) else ''}
-mean: {mean(self.resultsTwo)} #{'+'+str(meann) if meann > 0 else meann} {str(mean(self.resultsOne) / mean(self.resultsTwo) * 100 - 100)[:4]+'%' if mean(self.resultsOne) > mean(self.resultsTwo) else ''}
+max: {max(self.resultsTwo)} #{'+'+str(maks) if maks > 0 else maks} {'+'+str(max(self.resultsTwo) / max(self.resultsOne) * 100 - 100)[:4]+'%' if max(self.resultsTwo) > max(self.resultsOne) else ''}
+min: {min(self.resultsTwo)} #{'+'+str(minn) if minn > 0 else minn} {'+'+str(min(self.resultsTwo) / min(self.resultsOne) * 100 - 100)[:4]+'%' if min(self.resultsTwo) > min(self.resultsOne) else ''}
+mean: {mean(self.resultsTwo)} #{'+'+str(meann) if meann > 0 else meann} {'+'+str(mean(self.resultsTwo) / mean(self.resultsOne) * 100 - 100)[:4]+'%' if mean(self.resultsTwo) > mean(self.resultsOne) else ''}
 time for tests: {timer3}"""
         print(result)
         
